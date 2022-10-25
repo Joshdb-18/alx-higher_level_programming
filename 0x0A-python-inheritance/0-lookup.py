bd7c37edca01126d9a1d465e8482e734f9db2bf1
Joshdb-18 <joshuadami17@gmail.com>
2022-10-25 09:35:21 +0000

    0-lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,6 @@
+""" Function that retues tthe list of available attribuutes
+    and methods of an object
+"""
 #!/usr/bin/python3
 def lookup(obj):
     """ Function that returns the list of available attributes
