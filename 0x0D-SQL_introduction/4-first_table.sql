2cf0cb8984e78fab040159ce18c4ed78c05c6399
Joshdb-18 <joshuadami17@gmail.com>
2022-11-29 13:09:24 +0000

    create table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
--- creates a table calles first_table in the current database in yout MySQL server
-CREATE TABLE IF NOT EXISTS first_table (id INT, VARCHAR(256));
+-- creates a table called first_table in the current database in yout MySQL server
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
