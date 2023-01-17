db57f286530db2bc4fa8f8bc2960db5e1ba79802
Joshdb-18 <joshuadami17@gmail.com>
2023-01-17 16:22:52 +0100

    header response

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,6 +9,6 @@ import sys
 
 if __name__ == " __main__":
     url = sys.argv[1]
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
         print(dict(response.headers).get("X-Request-Id"))
