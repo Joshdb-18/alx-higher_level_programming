24271b8b2cec6bd5d1b73d50178a84c368f88f8a
Joshdb-18 <joshuadami17@gmail.com>
2023-01-17 15:50:47 +0100

    fetch htbn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,11 @@
 if __name__ == "__main__":
     import urllib.request
 
-    # req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as req:
-        content = req.read()
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
     print("Body response:")
     print('\t - type: {}'.format(type(content)))
     print('\t - content: {}'.format(content))
     print('\t - utf8 content: {}'.format(content.decode('UTF8')))
+
