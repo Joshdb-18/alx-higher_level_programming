783957d0e5cf40c6f6a5cdfbaa78fd451e2ede83
Joshdb-18 <joshuadami17@gmail.com>
2023-01-17 16:15:17 +0100

    response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Takes a URL and send a request to it and displays a value found in h response """
-if __name__ == " __main__":
-    import urllib.request
-    import sys
+"""Takes a URL and send a request to it and displays a value found in h response 
 
+Usage: ./1-hbtn_header.py <URL>
+"""
+
+import urllib.request, sys
+
+if __name__ == " __main__":
     url = sys.argv[1]
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
