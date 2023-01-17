51e14c2ad6c46adb9c64782bc07fa131b66e5fd3
Joshdb-18 <joshuadami17@gmail.com>
2023-01-17 15:57:26 +0100

    fetches hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(req) as response:
         content = response.read()
     print("Body response:")
-    print('\t - type: {}'.format(type(content)))
-    print('\t - content: {}'.format(content))
-    print('\t - utf8 content: {}'.format(content.decode('UTF8')))
+    print('\t- type: {}'.format(type(content)))
+    print('\t- content: {}'.format(content))
+    print('\t- utf8 content: {}'.format(content.decode('UTF8')))
