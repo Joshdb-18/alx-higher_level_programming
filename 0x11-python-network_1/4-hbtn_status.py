ff93272d284031b822e62bcfa83cffb3046c8ded
Joshdb-18 <joshuadami17@gmail.com>
2023-01-17 16:49:43 +0100

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-""" Using requests to fetch status."""
-
+"""Python script that fetches https://alx-intranet.hbtn.io/status
+using requests"""
 if __name__ == "__main__":
     import requests
 
-    req = requests.get('https://alx-intranet.hbtn.io.status')
-    print("Body response:")
-    print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format(req.text))
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print('Body response:\n\t- type: {}\n\t- content: {}'
+          .format(type(r.text), r.text))
